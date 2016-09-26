ed374087cf574694d2286d74bc6808446ec08ea8
Suprabht Paul <suprabhatpaul@gmail.com>
2016-09-26 13:29:40 +0530

    Unit Test Checked in

--- Atlas/Atlas/src/SystemFramework/Class1.cs
+++ Atlas/Atlas/src/SystemFramework/Class1.cs
@@ -9,6 +9,12 @@ namespace SystemFramework
     {
         public Class1()
         {
+
+        }
+
+        public int Add(int a, int b)
+        {
+            return (a + b);
         }
     }
 }
