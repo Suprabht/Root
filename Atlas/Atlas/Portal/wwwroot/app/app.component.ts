e6c9a4e73dee0f612d4b83f4699b9c8036e0c0a0
Suprabhat <suprabhatpaul@gmail.com>
2017-02-08 19:57:48 +0530

    bootstrap intrigration checked in

--- Atlas/Atlas/Portal/wwwroot/app/app.component.ts
+++ Atlas/Atlas/Portal/wwwroot/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1><ngb-alert [dismissible]="true">I'm a dismissible alert :) </ngb-alert>`,
 })
 export class AppComponent  { name = 'Angular'; }
