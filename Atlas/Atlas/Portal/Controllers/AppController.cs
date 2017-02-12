800afcf46d539ec83b9f71a33ae7658ae35a719d
Suprabhat <suprabhatpaul@gmail.com>
2017-02-12 22:20:18 +0530

    Enabling White space

--- Atlas/Atlas/Portal/Controllers/AppController.cs
+++ Atlas/Atlas/Portal/Controllers/AppController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Portal.Filters.CustomAttributes;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Portal.Controllers
 {
-   // [Route("App")]
+    [Route("Portal")]
+    [WhitespaceFilter]
     public class AppController : Controller
     {
         [Route("index")]
